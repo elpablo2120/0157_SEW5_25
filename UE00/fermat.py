f97437e12e1c678c00f2d93d46c4abb387127514
elpablo2120 <waldeckerpaul@gmail.com>
2024-09-23 16:16:29 +0200

    UE00 - A.1.2 - Creation miller_rabin.py - In Progress A.1.2 - Ready to Review A.1.1

--- UE00/fermat.py
+++ UE00/fermat.py
@@ -1,10 +1,10 @@
 """
 __author__ = "Paul Waldecker"
 __email__ = "0157@htl.rennweg.at"
-__version__ = "0.1"
+__version__ = "1.0"
 __copyright__ = "Copyright 2024"
 __license__ = "GPL"
-__status__ = "In Progress"
+__status__ = "Ready to Review"
 """
 from collections import Counter
 
@@ -29,7 +29,18 @@ def fermat(p):
     """
     return [pow(a, p - 1, p) for a in range(1, p)]
 
+
 def display(values, p):
+    """
+    Anzeige der Ergebnisse in Prozent und Anzahl der 1en in der Liste
+    :param values: Liste von fermat(p)
+    :param p: Primzahl bzw Zahl bei der getestet werden soll ob sie eine Primzahl ist
+    :return: Anzahl und Prozent der 1en in der Liste
+    >>> display([1, 1, 1, 1, 1, 1], 7)
+    7 -> 100.00 % -> res[1]=6, len(res)=6 - [(1, 6)]
+    >>> display(fermat(10), 10)
+    10 -> 11.11 % -> res[1]=1, len(res)=9 - [(1, 1), (2, 1), (3, 1), (4, 1), (5, 1), (6, 1), (7, 1), (8, 1), (9, 1)]
+    """
     counter = Counter(values)
     total = len(values)
     percentage = (counter[1] / total) * 100 if 1 in counter else 0
@@ -42,13 +53,13 @@ if __name__ == "__main__":
 
     # A.1.1 -------------
     primes = list(range(2, 12)) + [997]
-    non_primes = [9, 15, 21, 551, 552, 553, 554, 555, 556, 557, 558, 559, 560, 561, 562,
-                  563, 564, 565, 566, 567, 568, 569, 6601, 8911]
+    maybe_primes = [9, 15, 21, 551, 552, 553, 554, 555, 556, 557, 558, 559, 560, 561, 562,
+                    563, 564, 565, 566, 567, 568, 569, 6601, 8911]
 
     print("Ergebnisse für Primzahlen von 2 bis 11 und 997:")
     for p in primes:
         display(fermat(p), p)
 
     print("\nErgebnisse für Nicht-Primzahlen:")
-    for p in non_primes:
+    for p in maybe_primes:
         display(fermat(p), p)
