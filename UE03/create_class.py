a07b87df3102391d5b47bfe1aebb75cbf513b71f
elpablo2120 <waldeckerpaul@gmail.com>
2024-11-17 17:54:21 +0100

    UE03 - Funktion generate_random_password(length) implemented

--- UE03/create_class.py
+++ UE03/create_class.py
@@ -1,3 +1,4 @@
+import secrets
 import unicodedata
 
 def normalize_username(name: str) -> str:
@@ -7,6 +8,16 @@ def normalize_username(name: str) -> str:
     name = name.lower().replace(" ", "_")
     return ''.join(c for c in name if c.isalnum() or c == "_")
 
+#def generate_password(class_name, room_number, advisor) -> str:
+ #   special_chars = "!%&(),._-=^#"
+  #  random_char = secrets.choice(special_chars)
+   # return f"{class_name[0]}{random_char}{room_number[:3]}{advisor[0].upper()}"
+
+def generate_random_password(length=12) -> str:
+    chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!%&(),._-=^#"
+    return ''.join(secrets.choice(chars) for _ in range(length))
 
 if __name__ == "__main__":
     print(normalize_username("Hàns üßöä"))
+    print(generate_random_password())
+
