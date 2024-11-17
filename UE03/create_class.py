c76f3f4061b39fb9f88693bec079c5821fffac0f
elpablo2120 <waldeckerpaul@gmail.com>
2024-11-17 21:05:53 +0100

    UE03 - Changed Name of Funktion generate_password and generate_random_password to generate_password_student and generate_password_teacher_seminar

--- UE03/create_class.py
+++ UE03/create_class.py
@@ -35,16 +35,15 @@ def normalize_username(name: str) -> str:
     name = name.lower().replace(" ", "_")
     return ''.join(c for c in name if c.isalnum() or c == "_")
 
-def generate_password(class_name, room_number, advisor) -> str:
+def generate_password_student(class_name, room_number, advisor) -> str:
     special_chars = "!%&(),._-=^#"
     random_char = secrets.choice(special_chars)
     return f"{class_name[0]}{random_char}{room_number[:3]}{advisor[0].upper()}"
 
-def generate_random_password(length=12) -> str:
+def generate_password_teacher_seminar(length=12) -> str:
     chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!%&(),._-=^#"
     return ''.join(secrets.choice(chars) for _ in range(length))
 
 if __name__ == "__main__":
-    print(normalize_username("Hàns üßöä"))
-    print(generate_random_password())
+
 
