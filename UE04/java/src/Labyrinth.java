5e1f1721fe0581ac067a90225994f28db5d49a2d
elpablo2120 <waldeckerpaul@gmail.com>
2024-12-01 16:34:15 +0100

    UE04 - Implemented printLabyrinth

--- UE04/java/src/Labyrinth.java
+++ UE04/java/src/Labyrinth.java
@@ -113,7 +113,7 @@ public class Labyrinth {
 
 		return found;
 	}
-/*
+
 	/**
 	 * Ermittelt die Anzahl der Wege, die aus dem Labyrinth führen
 	 * @param zeile     aktuelle Position
@@ -122,12 +122,32 @@ public class Labyrinth {
 	 * @return anzahl Anzahl der unterschiedlichen Wege, die aus dem Labyrinth führen (ein bereits besuchtes Feld darf nicht nochmals betreten werden)
 	 * @throws InterruptedException    für die verlangsamte Ausgabe mit sleep()
 	 */
-	/*
 	public static int alleSuchen(int zeile, int spalte, char[][] lab) throws InterruptedException {
-		// TODO Code fehlt noch
+		// Checken ob A gefunden wurde
+		if (lab[zeile][spalte] == 'A') {
+			return 1;
+		}
+
+		// Checken ob eine Wand oder eine bereits besuchte Stelle gefunden wurde
+		if (lab[zeile][spalte] == '#' || lab[zeile][spalte] == '.') {
+			return 0;
+		}
+
+		// Die dezeite Stelle als besucht markieren
+		lab[zeile][spalte] = '.';
+
+
+		// Rekursiv nach dem Ausgang suchen
+		int anzahl = alleSuchen(zeile - 1, spalte, lab) // Up
+				+ alleSuchen(zeile + 1, spalte, lab) // Down
+				+ alleSuchen(zeile, spalte - 1, lab) // Left
+				+ alleSuchen(zeile, spalte + 1, lab); // Right
+		// aktuelle Stelle wieder freigeben
+		lab[zeile][spalte] = ' ';
+
 		return anzahl;
 	}
-*/
+
 
 	public static void main(String[] args) throws InterruptedException {
 		char[][] labyrinth = fromStrings(maps[0]);
