07098f58bb79233224a328c1e5a96a0f1fc1765c
elpablo2120 <waldeckerpaul@gmail.com>
2024-12-01 18:15:06 +0100

    UE04 - Implemented printLabyrinth

--- UE04/python/Labyrinth.py
+++ UE04/python/Labyrinth.py
@@ -1,10 +1,13 @@
-#fromStrings
-#printLabyrinth
+
 #suchen
 #alleSuchen
 #getLabyrinthFromFile
 
 def fromStrings(map: list[str]) -> list[list[str]]:
     return [list(x) for x in map]
+
+def printLabyrinth(lab: list[list[str]]) -> None:
+    for row in lab:
+        print("".join(row))
 
 
