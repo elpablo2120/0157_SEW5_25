c7135997fb2584dc85b44fe321e4f06521890d21
elpablo2120 <waldeckerpaul@gmail.com>
2024-12-01 18:25:55 +0100

    UE04 - Implemented suchen

--- UE04/python/Labyrinth.py
+++ UE04/python/Labyrinth.py
@@ -10,4 +10,24 @@ def printLabyrinth(lab: list[list[str]]) -> None:
     for row in lab:
         print("".join(row))
 
+def suchen(zeile: int, spalte: int, lab: list[list[int]]) -> bool:
+    if lab[zeile][spalte] == 'A':
+        return True
 
+    if lab[zeile][spalte] == '#' or lab[zeile][spalte] == '.':
+        return False
+
+    lab[zeile][spalte] = '.'
+
+    found = (suchen(zeile-1, spalte, lab) or
+                 suchen(zeile+1, spalte, lab) or
+                 suchen(zeile, spalte-1, lab) or
+                 suchen(zeile, spalte+1, lab))
+
+    lab[zeile][spalte] = ' '
+
+    return found
+
+def alleSuchen
+
+if 'name' == '__main__':
