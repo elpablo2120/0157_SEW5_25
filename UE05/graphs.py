a6550477ab2bf6834fda9331e73b892c869f66ae
elpablo2120 <waldeckerpaul@gmail.com>
2025-03-02 17:09:43 +0100

    UE05 - Klasse Edge Erstellen der Klassenattribute u, v, weight. Einbau von __str__.

--- UE05/graphs.py
+++ UE05/graphs.py
@@ -6,3 +6,25 @@ __copyright__ = "Copyright 2024"
 __license__ = "GPL"
 __status__ = "In Progress"
 """
+from dataclasses import dataclass
+
+
+@dataclass
+class Edge:
+    """
+    Eine Kante in einem gewichteten Graphen.
+    u und v sind die Indizes der Knoten, die die Kante verbindet (u=von, v=nach);
+    w = Gewicht der Kante
+    >>> e1 = Edge(1, 2, 99)
+    >>> e1
+    Edge(u=1, v=2, weight=99)
+    >>> print(e1)
+    1 --99-> 2
+    """
+    u: int
+    v: int
+    weight: int
+
+    def __str__(self) -> str:
+        return f"{self.u} --{self.weight}-> {self.v}"
+
