51c670bb35c83e8da0df04aa82531a1f77ba3619
elpablo2120 <waldeckerpaul@gmail.com>
2025-03-03 07:50:50 +0100

    UE05 - Klasse Graph Funktionen get_longest_shortest_path_in_graph und get_all_paths implementiert

--- UE05/graph.py
+++ UE05/graph.py
@@ -1,7 +1,7 @@
 """
 __author__ = "Paul Waldecker"
 __email__ = "0157@htl.rennweg.at"
-__version__ = "0.1"
+__version__ = "0.2"
 __copyright__ = "Copyright 2024"
 __license__ = "GPL"
 __status__ = "In Progress"
